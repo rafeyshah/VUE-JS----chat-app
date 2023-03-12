2abc142249ad47fb75336b2b3aa850bb3c807d31
Abdul Rafey <csyedabdulrafeyali@gmail.com>
2023-03-12 12:53:01 +0500

    Adding Login ( Layout + Functionality ) + DesignChat

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,61 @@
 <template>
-  <h1>Hello, World!</h1>
+  <div class="view login" v-if='this.state.username === "" || this.state.username === null'>
+    <form class="login-form" @submit.prevent="Login">
+      <div class="form-inner">
+        <h1>Login to FireChat</h1>
+        <label for="username" >Username</label>
+        <input 
+          type="text" 
+          v-model="inputUsername" 
+          placeholder="Please enter your username." />
+        <input 
+          type="submit" 
+          value="Login" />
+      </div>
+    </form>
+  </div>
+  <div class="view chat" v-else>
+    <header>
+      <button class="logout">Logout</button>
+      <h1>Welcome, {{ this.state.username }}</h1>
+    </header>
+    <section class="chat-box">
+      <!-- Messages -->
+      // Messages
+    </section>
+    <footer>
+      <form @submit.prevent="">
+        <input type="text" placeholder="Write a message"/>
+        <input type="submit" value="Send" />
+      </form>
+    </footer>
+  </div>
 </template>
 
 <script>
-import db from './db'
+import { ref } from 'vue'
+
+// import db from './db'
 export default {
 
+  data() {
+    return {
+      inputUsername: "",
+      state: {
+        username: "",
+        messages: []
+      }
+    }
+  },
+
+  methods: {
+    Login(){
+      if (this.inputUsername != "" || this.inputUsername != null){
+        this.state.username = this.inputUsername;
+        this.inputUsername = ""
+      }
+    }
+  }
 
 }
 </script>
@@ -239,4 +289,5 @@ export default {
       }
     }
   }
-}</style>
+}
+</style>
